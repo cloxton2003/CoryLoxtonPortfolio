f82acd1e83f317cd4644fbafb821091cbfb4f2a6
cloxton2003 <cloxton2003@hotmail.com>
2021-04-11 08:22:34 -0600

    reapplied jquery 3.0.0.min.js and also retyped email

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'CoryCodesCode@gmail.com>'; // Add Your Email
+$from = '<demo@domain.com>';
+$sendTo = '<corycodescode@gmail.com>'; // Add Your Email
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
