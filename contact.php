b0b2c12dc591a2ec4f4ba329aef727e038ba9ed4
cloxton2003 <cloxton2003@hotmail.com>
2021-04-11 12:54:52 -0600

    configured from email as websites domain name

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure
-$from = '<demo@domain.com>';
+$from = '<contact@corycodescode.com>';
 $sendTo = '<corycodescode@gmail.com>'; // Add Your Email
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
